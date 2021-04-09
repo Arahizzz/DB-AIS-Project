b7c523ff0c83fe552b040872772df3517196c96e
DESKTOP-Q7R713D\Vadym <vadim.nakitnyak@gmail.com>
2021-04-09 15:49:25 +0300

    Add category add page, OnPost not working

---
+++ DBAIS/Pages/CategoryPages/CategoryAdd.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBAIS.Models;
+using DBAIS.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DBAIS.Pages
+{
+    public class CategoryAddModel : PageModel
+    {
+        private readonly CategoryRepository _categoryRepository;
+        public string Message { get; set; }
+
+        public CategoryAddModel(CategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        public void OnGet()
+        {
+            Message = "Get";
+        }
+        public async Task<IActionResult> OnPostAsync([FromForm] string categoryName)
+        {
+            Message = "Post";
+            var newCategory = new Category { Name = categoryName };
+            await _categoryRepository.AddCategory(newCategory);
+            return Redirect("/category");
+        }
+    }
+}
