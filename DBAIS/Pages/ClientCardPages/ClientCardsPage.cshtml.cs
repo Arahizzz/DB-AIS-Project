7eff9770120aca01a28e6e22f5ecfc1cc849528c
DESKTOP-Q7R713D\Vadym <vadim.nakitnyak@gmail.com>
2021-04-10 14:23:37 +0300

    Added customers page

---
+++ DBAIS/Pages/ClientCardPages/ClientCardsPage.cshtml.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DBAIS.Models;
+using DBAIS.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DBAIS.Pages.ClientCardPages
+{
+    public class ClientCardsPageModel : PageModel
+    {
+        private readonly CustomerRepository _customerRepository;
+
+        public ClientCardsPageModel(CustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public List<Card> Customers { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Customers = new List<Card>();
+            Customers.Add(new Card{ Number = 12345, Surname = "S", Name = "N", Patronymic = "P", Phone = "+380", City = "Kyiv", Percent = 5});
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            Customers = new List<Card>();
+            return Page();
+        }
+    }
+}
