c706aba4b77a9c63198ac68f2ea7cafbd91581d0
DESKTOP-Q7R713D\Vadym <vadim.nakitnyak@gmail.com>
2021-04-11 11:48:03 +0300

    Fixed logout

--- DBAIS/Pages/Index.cshtml.cs
+++ DBAIS/Pages/Index.cshtml.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DBAIS.Models;
 using DBAIS.Models.DTOs;
 using DBAIS.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,10 +14,14 @@ namespace DBAIS.Pages
     public class IndexModel : PageModel
     {
         private readonly CheckRepository _customerRepository;
+        private readonly SignInManager<EmployeeUser> _signInManager;
+        private readonly UserManager<EmployeeUser> _userManager;
 
-        public IndexModel(CheckRepository customerRepository)
+        public IndexModel(CheckRepository customerRepository, UserManager<EmployeeUser> userManager, SignInManager<EmployeeUser> signInManager)
         {
             _customerRepository = customerRepository;
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         public List<PurchaseInfo> Purchases { get; set; } = null!;
@@ -23,6 +29,14 @@ namespace DBAIS.Pages
         public async Task OnGetAsync()
         {
             Purchases = await _customerRepository.GetCustomerChecks("7121520341739");
+        }
+
+        public async Task<IActionResult> OnPostLogoutAsync()
+        {
+            await _signInManager.SignOutAsync();
+            //_userManager.GetUserAsync
+            //await _userManager.GetRolesAsync();
+            return Redirect("/");
         }
     }
 }
